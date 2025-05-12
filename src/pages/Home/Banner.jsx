d3f359632937f38e1ec6f555af65f561b969bd6c
Nafiuls <snafiul700@gmail.com>
2025-05-12 21:48:29 +0600

    banner don alhamdulillah

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import bg from "../../assets/bg.png";
+import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <div
@@ -10,7 +11,24 @@ const Banner = () => {
         height: "650px", // or any height you want
         width: "100%",
       }}
-    ></div>
+    >
+      {/* content */}
+      <div className="flex items-center flex-col justify-center h-full">
+        <h1 className="text-7xl leading-20 lg:px-10 text-center font-extrabold">
+          Game Reviews, <br /> Minus the Noise
+        </h1>
+        <p className="max-w-3xl text-center text-xl mt-5">
+          "Find honest reviews, chill discussions, and the games that suit your
+          vibe. Whether you're a casual player or a late-night grinder, we’ve
+          built this space for you"
+        </p>
+        <Link to={"/reviews"}>
+          <button className="btc mt-5 text-xl font-medium transition-all cursor-pointer px-10 py-2 rounded-sm">
+            Browse Reviews
+          </button>
+        </Link>
+      </div>
+    </div>
   );
 };
 
